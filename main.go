9d9b32482331d83d98d2c98c5ef6db7abb02c515
Fabio Simone Sebastiano <sebastiano.fabio@gmail.com>
2021-07-21 12:29:50 +0200

    Creato  loop per generare 3 S3 bucket

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create an AWS resource (S3 Bucket)
-		bucket, err := s3.NewBucket(ctx, "my-bucket", nil)
-		if err != nil {
-			return err
+		bucketList := []string{"bucket-pulumi-1", "bucket-pulumi-2", "bucket-pulumi-3"}
+
+		for i, v := range bucketList {
+
+			// Create an AWS resource (S3 Bucket)
+			bucket, err := s3.NewBucket(ctx, v, nil)
+			if err != nil {
+				return err
+			}
+
+			// Export the name of the bucket
+			ctx.Export("bucketName_"+strconv.Itoa(i), bucket.ID())
 		}
 
-		// Export the name of the bucket
-		ctx.Export("bucketName", bucket.ID())
 		return nil
 	})
 }
